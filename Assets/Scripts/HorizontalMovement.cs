9f05faffda7aff351364ab7532a11959347d746e
PC-imatge-2022-2023 <alejandro.lopez@enti.cat>
2023-01-30 10:13:24 +0100

    Horizontal Movement Done

---
+++ Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontal * speed * Time.fixedDeltaTime, 0, 0);
+
+    }
+}
