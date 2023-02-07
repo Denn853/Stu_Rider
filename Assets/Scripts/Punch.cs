5d4a943a13b41548ff5a0dad2758bc310cafd714
PC-ENTI <alejandro.lopez@enti.cat>
2023-02-07 11:13:45 +0100

    Starting puch contact particle system

--- Assets/Scripts/Punch.cs
+++ Assets/Scripts/Punch.cs
@@ -5,44 +5,22 @@ using UnityEngine;
 public class Punch : MonoBehaviour
 {
 
-    public bool canPuch = true;
-    public float punchTime;
-    //public float coolDownTime;
-    public GameObject arm;
+    //public GameObject punchParticles;
 
-    SpriteRenderer sr;
     CapsuleCollider2D coll;
 
     // Start is called before the first frame update
     void Start()
     {
-        sr = arm.GetComponent<SpriteRenderer>();
-        coll = arm.GetComponent<CapsuleCollider2D>();
+        coll = GetComponent<CapsuleCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    { 
-        if (Input.GetButtonDown("Punch") && canPuch)
-        {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy") {
 
-            StartCoroutine(Punch_Corutine());
+            Destroy(collision.gameObject);
 
         }
-    }
-
-    IEnumerator Punch_Corutine()
-    {
-
-        sr.enabled = true;
-        coll.enabled = true;
-        canPuch = false;
-
-        yield return new WaitForSeconds(punchTime);
-
-        sr.enabled = false;
-        coll.enabled = false;
-        canPuch = true;
-
     }
 }
