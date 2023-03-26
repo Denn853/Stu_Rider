e80d40621fee3209e737b1222a42b45087c2eebb
Alejandro Lopez <alejandro.lopez@enti.cat>
2023-03-26 10:17:24 +0200

    Hook & enemy1 attacks on both directions

--- Assets/Scripts/Abilities/Hook.cs
+++ Assets/Scripts/Abilities/Hook.cs
@@ -4,28 +4,61 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-
-    public bool hook = false;
+    [Header("Hook Settings")]
+    public Transform startPosition;
     public float speedHook;
+    public float speedToArrive;
     public float hookDistance = 5;
     public LayerMask hookMask;
     public List<Vector3> rays;
 
+    [Header("Hook Status")]
+    public bool hook = false;
+    public bool isGrappling = false;
+
+    bool isRetracting;
     float offset;
     float angle;
     Vector3 dir;
     Vector3 positionToMove;
+    LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = 360 / rays.Count;
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        CheckPlatforms();
+
+        if (Input.GetButtonDown("Hook") && hook && !isGrappling)
+        {
+            StartGrappling();
+        }
+
+        if (isRetracting)
+        {
+            Vector2 grapplePosition = Vector2.Lerp(transform.position, positionToMove, speedToArrive * Time.deltaTime);
+            transform.position = grapplePosition;
+
+            lineRenderer.SetPosition(0, transform.position);
+
+            if (Vector2.Distance(transform.position, positionToMove) < 0.5f)
+            {
+                isRetracting = false;
+                isGrappling = false;
+                lineRenderer.enabled = false;
+            }
+        }
+    }
+
+    private void CheckPlatforms ()
+    {
         for (int i = 0; i < rays.Count; i++)
         {
             angle = i * offset;
@@ -33,22 +66,48 @@ public class Hook : MonoBehaviour
             dir = Quaternion.Euler(0, 0, angle) * transform.right;
             Debug.DrawRay(transform.position, dir * hookDistance, Color.red);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, hookDistance, hookMask);
-        
+
             if (hit.collider != null)
             {
                 Debug.DrawRay(transform.position, dir * hit.distance, Color.cyan);
                 positionToMove = hit.transform.position;
-                hook = true;             
+                hook = true;
                 break;
             }
 
             hook = false;
-        
+
+        }
+    }
+
+    private void StartGrappling()
+    {
+        isGrappling = true;
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = 2;
+
+        StartCoroutine(Grapple());
+    }
+
+    IEnumerator Grapple()
+    {
+        float timer = 0;
+        float time = 10;
+
+        lineRenderer.SetPosition(0, startPosition.position);
+        lineRenderer.SetPosition(1, startPosition.position);
+
+        Vector2 newPos;
+
+        for (; timer < time; timer += speedHook * Time.deltaTime) {
+            newPos = Vector2.Lerp(startPosition.position, positionToMove, timer / time);
+            lineRenderer.SetPosition(0, startPosition.position);
+            lineRenderer.SetPosition(1, newPos);
+            
+            yield return null;
         }
 
-        if (Input.GetButtonDown("Hook") && hook)
-        {
-            transform.position = Vector3.Lerp(transform.position, positionToMove, speedHook * Time.deltaTime);
-        }
+        lineRenderer.SetPosition(1, positionToMove);
+        isRetracting = true;
     }
 }
--- Assets/Scripts/Enemies/EnemyHorizontalMovement.cs
+++ Assets/Scripts/Enemies/EnemyHorizontalMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyHorizontalMovement : MonoBehaviour
 {
+
+    public enum Directions { NONE, RIGHT, LEFT };
+
     [Header("Movement Settings")]
     public List<Transform> points;
     public int nextPoint = 0;
@@ -11,13 +14,16 @@ public class EnemyHorizontalMovement : MonoBehaviour
     public float distance;
 
     [Header("Movement Status")]
-    public float direction;
+    public Directions direction;
+
+    [Header("Attack Position")]
+    public GameObject attack;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = points[nextPoint].position;
-        direction = 1;
+        direction = Directions.LEFT;
     }
 
     // Update is called once per frame
@@ -31,11 +37,15 @@ public class EnemyHorizontalMovement : MonoBehaviour
 
         if (nextPoint == 0)
         {
-            direction = 1f;
+            direction = Directions.RIGHT;
+            attack.transform.position = new Vector3(transform.position.x + 0.85f, transform.position.y, transform.position.z);
+            //sr.flipX = true;
         }
         else
         {
-            direction = -1f;
+            direction = Directions.LEFT;
+            attack.transform.position = new Vector3(transform.position.x - 0.85f, transform.position.y, transform.position.z);
+            //sr.flipX = false;
         }
 
         if (distance < 0.5f)
