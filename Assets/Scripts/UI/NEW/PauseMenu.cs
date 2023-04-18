bace88c5be730aec562f3f0f0b91721eb381e231
Alejandro Lopez <alejandro.lopez@enti.cat>
2023-04-18 18:46:07 +0200

    Pause Menu working

---
+++ Assets/Scripts/UI/NEW/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [Header("Pause Menu")]
+    [SerializeField] private GameObject pauseMenu;
+
+    [Header("Pause Status")]
+    [SerializeField] private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && !isPaused)
+        {
+            ShowPauseMenu();
+        }
+        else if (Input.GetButtonDown("Cancel") && isPaused)
+        {
+            HidePauseMenu();
+        }
+    }
+
+    public void ShowPauseMenu()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void HidePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+}
