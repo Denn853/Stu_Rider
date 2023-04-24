9e0a1b96637ba71acb7c50d5c5b64324c19369d3
PC-ENTI-Alejandro <alejandro.lopez@enti.cat>
2023-04-24 11:24:59 +0200

    Audio settings on Options Menu working

---
+++ Assets/Scripts/UI/NEW/SettingsMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    [Header("Audio Mixer References")]
+    public AudioMixer mainAudioMixer;
+    public AudioMixer sfxAudioMixer;
+
+    [Header("Not interactable")]
+    public Toggle muteToggle;
+    public Slider mainSliderBar;
+    public Slider sfxSliderBar;
+
+
+    
+    public void SetMainVolume(float volume)
+    {
+        mainAudioMixer.SetFloat("mainVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxAudioMixer.SetFloat("sfxVolume", volume);
+    }
+
+    public void MuteAll()
+    {
+        SetMainVolume(-80.0f);
+        SetSFXVolume(-80.0f);
+
+        if (muteToggle.isOn)
+        {
+            mainSliderBar.interactable = false;
+            sfxSliderBar.interactable = false;
+        }
+        else
+        {
+            mainSliderBar.interactable = true;
+            sfxSliderBar.interactable = true;
+        }
+    }
+
+}
