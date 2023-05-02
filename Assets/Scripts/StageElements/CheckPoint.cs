2819330472d8bec52e626fa7b83dafbb50cc5c0e
DennysEnti <dennys.hernandez@enti.cat>
2023-05-02 22:01:39 +0200

    Trying to put animations in player......, Checkpoint animation DONE

--- Assets/Scripts/StageElements/CheckPoint.cs
+++ Assets/Scripts/StageElements/CheckPoint.cs
@@ -7,10 +7,19 @@ public class CheckPoint : MonoBehaviour
     [Header("Audio")]
     [SerializeField] private AudioSource checkPointSoundEffect;
 
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         checkPointSoundEffect.Play();  
 
         PlayerController.instance.CheckPoint(transform.position);
+
+        anim.SetTrigger("isActive");
     }
 }
