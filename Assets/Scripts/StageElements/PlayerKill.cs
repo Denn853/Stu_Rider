f70b09f211087070f3fde9fbd4e96bd13ce44f56
PC-ENTI-Alejandro <alejandro.lopez@enti.cat>
2023-05-04 09:45:08 +0200

    Main Menu Controls Menu OK

--- Assets/Scripts/StageElements/PlayerKill.cs
+++ Assets/Scripts/StageElements/PlayerKill.cs
@@ -13,7 +13,6 @@ public class PlayerKill : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Contact");
         StartCoroutine(DealDamageCorrutine());
     }
 
