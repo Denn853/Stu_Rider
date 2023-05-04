9b1f8502f235e1aee1de0f67033a053e34c3c486
PC-ENTI-Alejandro <alejandro.lopez@enti.cat>
2023-05-04 09:42:50 +0200

    Error error kill Level_!

--- Assets/Scripts/StageElements/PlayerKill.cs
+++ Assets/Scripts/StageElements/PlayerKill.cs
@@ -13,6 +13,7 @@ public class PlayerKill : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Contact");
         StartCoroutine(DealDamageCorrutine());
     }
 
