bd66ff5f9ad36e4f1e4cde8f3d60b7fc6793d4f5
Alejandro Lopez <alejandro.lopez@enti.cat>
2023-05-06 10:29:40 +0200

    Sliders working fine :)

--- Assets/Scripts/UI/NEW/SettingsMenu.cs
+++ Assets/Scripts/UI/NEW/SettingsMenu.cs
@@ -16,34 +16,60 @@ public class SettingsMenu : MonoBehaviour
     public Toggle muteToggle;
     public Slider mainSliderBar;
     public Slider sfxSliderBar;
-        
+
+    float volumeMusic;
+    float volumeSFX;
+
+    float minVolumeMusic;
+    float minVolumeSFX;
+
+    private void Start()
+    {
+        minVolumeMusic = -40.0f;
+        minVolumeSFX = -40.0f;
+
+        mainAudioMixer.GetFloat("mainVolume", out volumeMusic);
+        mainAudioMixer.GetFloat("sfxVolume", out volumeMusic);
+
+        mainSliderBar.value = volumeMusic;
+        sfxSliderBar.value = volumeSFX;
+    }
     public void SetMainVolume(float volume)
     {
         mainAudioMixer.SetFloat("mainVolume", volume);
+        volumeMusic = volume;
     }
 
     public void SetSFXVolume(float volume)
     {
         sfxAudioMixer.SetFloat("sfxVolume", volume);
+        volumeSFX = volume;
     }
 
     public void MuteAll()
     {
-        SetMainVolume(-80.0f);
-        SetSFXVolume(-80.0f);
-        
-        mainSliderBar.value = -80.0f;
-        sfxSliderBar.value = -80.0f;
 
         if (muteToggle.isOn)
         {
             mainSliderBar.interactable = false;
             sfxSliderBar.interactable = false;
+
+            SetMainVolume(minVolumeMusic);
+            SetSFXVolume(minVolumeSFX);
+
+            mainSliderBar.value = minVolumeMusic;
+            sfxSliderBar.value = minVolumeSFX;
         }
         else
         {
             mainSliderBar.interactable = true;
             sfxSliderBar.interactable = true;
+
+            SetMainVolume(volumeMusic);
+            SetSFXVolume(volumeSFX);
+
+            mainSliderBar.value = volumeMusic;
+            sfxSliderBar.value = volumeSFX;
         }
     }
 }
