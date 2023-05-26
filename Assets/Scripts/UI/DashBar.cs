2d596c0cdd74c15fb0041b705c1894c9a25661f3
Alejandro Lopez <alejandro.lopez@enti.cat>
2023-05-26 15:00:15 +0200

    Dashbar solved?

--- Assets/Scripts/UI/DashBar.cs
+++ Assets/Scripts/UI/DashBar.cs
@@ -10,54 +10,43 @@ public class DashBar : MonoBehaviour
     [SerializeField] private List<Image> dashBar = new List<Image>();
     float timeToVoid;
     float timeToFill;
-    bool isVoiding;
-    bool isFilling;
 
     private void Start()
     {
-        isVoiding = false;
-        isFilling = true;
 
         timeToVoid = dash.dashingTime / (200 * dashBar.Count);
         timeToFill = dash.coolDownTime / (200 * dashBar.Count);
+
     }
 
     private void Update()
     {
-        if (!dash.canDash && !isVoiding)
+        if (!dash.canDash)
         {
-            StartCoroutine(VoidBar());
+            VoidBar();
         }
 
-        if (dash.cooldown && !isFilling)
+        if (dash.cooldown)
         {
             StartCoroutine(FillBar());
         } 
     }
 
-    IEnumerator VoidBar()
+    void VoidBar()
     {
-        isVoiding = true;
-
-        for (int i = dashBar.Count - 1; i >= 0; i-= 2)
+        for (int i = dashBar.Count - 1; i >= 0; i--)
         {
             dashBar[i].fillAmount = -1;
-
-            if (i - 1 >= 0)
-                dashBar[i - 1].fillAmount = -1;
-            yield return new WaitForSeconds(timeToVoid);
         }
-
-        isFilling = false;
     }
+
 
     IEnumerator FillBar()
     {
-        isFilling = true;
 
         yield return new WaitForSeconds(timeToVoid);
 
-        for (int i = 0; i <= dashBar.Count - 2; i+= 3)
+        for (int i = 0; i <= dashBar.Count - 2; i += 3)
         {
             dashBar[i].fillAmount = 1;
             dashBar[i + 1].fillAmount = 1;
@@ -68,6 +57,5 @@ public class DashBar : MonoBehaviour
 
         dashBar[dashBar.Count - 1].fillAmount = 1;
 
-        isVoiding = false;
     }
 }
