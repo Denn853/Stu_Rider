f41c4d7c1a484b6aeb8de7496a03c050392519bc
Alejandro Lopez <alejandro.lopez@enti.cat>
2023-05-29 15:40:09 +0200

    Immortality and solving MiniSaw collider

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,8 @@ public class GameManager : MonoBehaviour
 
     public bool introPlayed;
 
+    public bool isImmortal;
+
     private void Awake()
     {
         if (instance == null)
@@ -34,7 +36,9 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         lifes = 3;
-        introPlayed = false;
+        introPlayed = false
+            ;
+        isImmortal = false;
         //level = 1;
     }
 
